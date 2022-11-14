6eefcb2985657ecc8106f34a736e977a863f5b78
min22222 <109497355+min22222@users.noreply.github.com>
2022-11-15 03:02:35 +0900

    레이아웃 수정

---
+++ app/src/main/java/edu/sungshin/bookstorming/GalleryActivity.java
@@ -0,0 +1,49 @@
+package edu.sungshin.bookstorming;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class GalleryActivity extends AppCompatActivity {
+
+    private  final int GET_GALLERY_IMAGE =200;
+    ImageView ivProfile;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+        ivProfile = findViewById(R.id.ivProfile);
+
+        ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+                startActivityForResult(intent,GET_GALLERY_IMAGE);
+
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_GALLERY_IMAGE&&resultCode==RESULT_OK&&data!=null
+        &&data.getData()!=null) {
+            Uri selectedImageUri = data.getData();
+            ivProfile.setImageURI(selectedImageUri);
+        }
+    }
+}
+
+
--- app/src/main/java/edu/sungshin/bookstorming/MainActivity.java
+++ app/src/main/java/edu/sungshin/bookstorming/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Toast;
 
